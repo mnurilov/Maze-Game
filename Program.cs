d0baf54f427b5bc7be5ac6d0868e0db78e6332e9
Michael Nurilov <33560995+mnurilov@users.noreply.github.com>
2018-04-22 13:14:51 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Game
+{
+    class Wall
+    { 
+        public char display = '#';
+        public int length = 18;
+        public int width = 48;
+        
+        public void Draw(string s)
+        {
+            for (int row = 0; row < length; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    if (col == 0 || col == width - 1 || row == 0 || row == length - 1)
+                    { 
+                        Console.Write( display);
+                    }
+                    else 
+                    { 
+                        Console.Write(" ");    
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        public static string Buffer = "";
+
+        static void DisplayMap() 
+        {
+            Console.Write(Buffer);  
+        }
+        static void Main(string[] args)
+        {
+            Wall Wall = new Wall();
+            Wall.Draw(Buffer);
+            DisplayMap();
+            Console.ReadLine();
+        }
+    }
+}
