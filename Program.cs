62c5827fdf8b8bd0332aa29aed9a4ad2fdcf452c
Michael Nurilov <33560995+mnurilov@users.noreply.github.com>
2018-06-21 13:17:57 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,49 +3,311 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Maze_Game
 {
-    class Wall
-    { 
-        public char display = '#';
-        public int length = 18;
-        public int width = 48;
-        
-        public void Draw(string s)
-        {
+    //A class used to make the buffer and game map global for all other classes to use
+    static class Buffer 
+    {
+        //Length and width of the game map
+        public static int length = 20;
+        public static int width = 43;
+
+        //Storages of the game map the char array which holds the logic game map and the string which is for visuals
+        public static string buffer = "";
+        public static char [,] gameMap = new char [length, width];
+
+        public static void GameMapToBuffer()
+        { 
+            //Reset buffer string to empty
+            buffer = "";
+
+            //Nested for loops to store the game map array into the buffer string
             for (int row = 0; row < length; row++)
             {
                 for (int col = 0; col < width; col++)
                 {
-                    if (col == 0 || col == width - 1 || row == 0 || row == length - 1)
-                    { 
-                        Console.Write( display);
-                    }
-                    else 
-                    { 
-                        Console.Write(" ");    
-                    }
+                    buffer += gameMap[row, col];
                 }
-                Console.WriteLine();
+                buffer += '\n';
+            }
+        }
+
+        //Draws the game map onto the console window
+        public static void DrawGameMap() 
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write(buffer);    
+        }
+    }
+
+    //The player class which holds the logic for the player
+    class Player
+    {
+        //Variables that hold the player's display
+        public char display = '$';
+
+        //Variables to control where the player starts
+        private static int startingx = 1;
+        private static int startingy = 18;
+
+        //Variables that hold the player's current position
+        public int x = startingx;
+        public int y = startingy;
+
+        //Variables that hold the player's previous position
+        public int previousx = startingx;
+        public int previousy = startingy;
+
+        //directional movements
+        public void MoveUp() 
+        {
+            previousy = y;
+            previousx = x;
+            y--;
+        }
+        public void MoveDown() 
+        {
+            previousy = y;
+            previousx = x;
+            y++;
+        }
+        public void MoveLeft() 
+        {
+            previousx = x;
+            previousy = y;
+            x--;    
+        }
+        public void MoveRight() 
+        { 
+            previousx = x;
+            previousy = y;
+            x++;    
+        }
+
+        //chooses the direction that the player goes
+        public void Direction(ConsoleKey direction)
+        {
+            switch(direction)
+            { 
+                case ConsoleKey.UpArrow:
+                    MoveUp();
+                    break;
+                case ConsoleKey.DownArrow:
+                    MoveDown();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    MoveLeft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    MoveRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //Draws the player on the game map at its current position
+        public void DrawToGameMap() 
+        {    
+            //y and x are swapped so that it plots correctly on the game map just pretend its like normal coords
+            Buffer.gameMap[y, x] = display;
+        }
+
+        //Deletes the player on the game map at its previous position
+        public void Delete() 
+        {
+            //y and x are swapped so that it plots correctly on the game map just pretend its like normal coords
+            Buffer.gameMap[previousy, previousx] = ' ';
+        }
+
+        //Collision detection for the player
+        public void PlayerCollision() 
+        {
+            if (Buffer.gameMap[y, x] == '#')
+            {
+                x = previousx;
+                y = previousy;
+            }
+            else if (Buffer.gameMap[y, x] == 'U')
+            {
+                Environment.Exit(0);    
             }
         }
     }
 
+    //Wall class to hold the game logic for the wall
+    static class Wall
+    {
+        //Variables that hold the wall's display 
+        public static char display = '#';
+       
+        //A function that draws the walls onto the game map
+        public static void DrawToGameMap()
+        {
+            for (int row = 0; row < Buffer.length; row++)
+            {
+                for (int col = 0; col < Buffer.width; col++)
+                {
+                    if (col == 0 || col == Buffer.width - 1 || row == 0 || row == Buffer.length - 1)
+                    {
+                        Buffer.gameMap[row, col] = '#';
+                    }
+                    else
+                    {
+                        Buffer.gameMap[row, col] = ' ';
+                    }
+                }
+            }
+            for (int i = 16; i < 19; i++) 
+            {
+                Buffer.gameMap[i, 5] = '#';    
+            }
+
+            for (int i = 0; i < 37; i++)
+            { 
+                Buffer.gameMap[14, i] = '#'; 
+            }
+        }
+    }
+
+    //Class for the up down enemy
+    class UpDownEnemy 
+    { 
+        //Display character for the up down enemy
+        public char display = 'U';
+
+        //Current coordinates for the up down enemy
+        public int x;
+        public int y;
+
+        //Previous coordinates for the up down enemy
+        public int previousx;
+        public int previousy;
+
+        //Boolean to check if the up down enemy is going up or down
+        public bool goingUp = true;
+
+        //Method to make the up down enemy go up or down
+        public void Move() 
+        {
+            if (goingUp == true)
+            { 
+                previousx = x;
+                previousy = y;
+                y--;
+            }
+            else 
+            {
+                previousx = x;
+                previousy = y;
+                y++;    
+            }
+        }
+
+        public void UpDownEnemyCollision() 
+        {
+            if (Buffer.gameMap[y, x] == '#')
+            {
+                x = previousx;
+                y = previousy;
+                goingUp = !goingUp;
+            }
+            else if (Buffer.gameMap[y, x] == '$')
+            { 
+                Environment.Exit(0);
+            }
+        }
+
+        public void DrawToGameMap() 
+        {
+            Buffer.gameMap[y, x] = display;    
+        }
+
+        public void Delete() 
+        {
+            Buffer.gameMap[previousy, previousx] = ' ';    
+        }
+
+        public UpDownEnemy(int startingx1, int startingy1, bool upOrDown) 
+        {
+            x = startingx1;
+            y = startingy1;
+            previousx = startingx1;
+            previousy = startingy1;
+            goingUp = upOrDown;
+        }
+    }
+
+    //Main class
     class Program
     {
-        public static string Buffer = "";
+        static object Baton = new object();
 
-        static void DisplayMap() 
+        static void SetUpConsoleWindow() 
         {
-            Console.Write(Buffer);  
+            //Sets up the basics of the console window
+            Console.Title = "Maze Game";
+            Console.CursorVisible = false;
         }
+
+        static void Enemies() 
+        {           
+            UpDownEnemy upDownEnemy1 = new UpDownEnemy(5, 5, true);
+
+            while (true) 
+            {
+                upDownEnemy1.Move();
+                upDownEnemy1.UpDownEnemyCollision();
+                upDownEnemy1.Delete();
+                upDownEnemy1.DrawToGameMap();
+                lock (Baton) 
+                {
+                    Buffer.GameMapToBuffer();
+                    Buffer.DrawGameMap();
+                }
+                Thread.Sleep(250);
+            }
+        }
+        //Main program
         static void Main(string[] args)
         {
-            Wall Wall = new Wall();
-            Wall.Draw(Buffer);
-            DisplayMap();
-            Console.ReadLine();
+            SetUpConsoleWindow();
+
+            ConsoleKey input;
+            Player Player = new Player();
+            UpDownEnemy upDownEnemy1 = new UpDownEnemy(5, 5, true);
+            Wall.DrawToGameMap();
+            Player.DrawToGameMap();
+            upDownEnemy1.DrawToGameMap();
+            Buffer.GameMapToBuffer();
+            Buffer.DrawGameMap();
+
+            Thread EnemyThread = new Thread(Enemies);
+            EnemyThread.Start();
+            
+           
+
+            //Game Loop
+            while (true)
+            {
+                while (!Console.KeyAvailable) 
+                {
+                }
+                input = Console.ReadKey().Key;
+
+                Player.Direction(input);
+                Player.PlayerCollision();
+                Player.Delete();
+                Player.DrawToGameMap();
+                lock (Baton)
+                {
+                    Buffer.GameMapToBuffer();
+                    Buffer.DrawGameMap();
+                }
+
+            }
         }
     }
 }
